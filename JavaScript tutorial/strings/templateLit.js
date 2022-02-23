e5ae7a40c20401d5930816f6f86aa1dd2c510835
Avijit Biswas <avijit.biswas27@outlook.com>
2022-02-23 18:25:43 +0530

    storing information

---
+++ JavaScript tutorial/strings/templateLit.js
@@ -0,0 +1,7 @@
+const song = "Fight the Youth";
+const score = 9;
+const highestScore = 10;
+const output = `I like the song ${song}. I gave it a score of ${
+    (score / highestScore) * 100
+}%.`;
+console.log(output); // "I like the song Fight the Youth. I gave it a score of 90%."
